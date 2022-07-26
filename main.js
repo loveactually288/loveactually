b559b2ef6a688d2ab35334553ef30f2a771a91a8
loveactually288 <110028752+loveactually288@users.noreply.github.com>
2022-07-26 23:07:19 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,8 @@ console.clear();
 http.__okhttp__.setTimeout(10000);
 
 let url = [
-    'https://cdn.sec-an.cn/d/storage01/Better-Auto-XXQG/UI.js?sign=a088332432834f29',
-    'https://sp.sec-an.cn/storage01/Better-Auto-XXQG/UI.js',
-    'https://ghproxy.com/https://raw.githubusercontent.com/sec-an/Better-Auto-XXQG/main/UI.js',
-    'https://cdn.jsdelivr.net/gh/sec-an/Better-Auto-XXQG@main/UI.js',
-    'https://raw.githubusercontent.com/sec-an/Better-Auto-XXQG/main/UI.js',
+    'https://raw.githubusercontent.com/loveactually288/loveactually/main/UI.js',
+    'http://cdn.jsdelivr.net/china/loveactually288/loveactually@3.0/UI.js',
 ];
 
 for (var i = 0; i < url.length; i++) {
